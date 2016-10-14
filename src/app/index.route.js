688d26924539ed37ff3074f47dd6ddacd18b6b4d
suyusama <483644331@qq.com>
2016-10-14 08:30:58 +0800

    config

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -4,18 +4,29 @@
   angular
     .module('asset')
     .config(routerConfig);
-
   /** @ngInject */
   function routerConfig($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainController',
-        controllerAs: 'main'
-      });
+    $stateProvider.state('login', {
+        url: '/login',
+        templateUrl: 'app/template/login2.template.html',
+      }).state("signIn",{
+        url:"/assets",
+        templateUrl:'app/template/assets.template.html'
+      }).state("assets",{
+        url:"/assets",
+        templateUrl:'app/template/assets.template.html'
+      }).state("buildings",{
+        url:"/buildings",
+        templateUrl:'app/template/buildings.template.html'
+      }).state("equipments",{
+        url:"/equipments",
+        templateUrl:'app/template/equipments.template.html'
+      }).state("rooms",{
+        url:"/rooms",
+        templateUrl:'app/template/rooms.template.html'
+      })
 
-    $urlRouterProvider.otherwise('/');
+    // $urlRouterProvider.otherwise('/h/login');
   }
-
+  
 })();
