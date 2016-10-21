920660efe3d368535da855820614ed326251caa9
liliyanjun <15600555067@163.com>
2016-10-21 11:59:05 +0800

    asad

--- src/app/controller/asset.rooms.controller.js
+++ src/app/controller/asset.rooms.controller.js
@@ -1,53 +1,39 @@
-angular.module('asset').controller("assetRooms",function($scope,$http,environment,apiServ){
+angular.module('asset').controller("assetRooms",function($scope,accountServ,$http,environment,apiServ){
   $scope.pageData = [];
   function _slice(arr, page, number){
     return arr.slice((page-1)*number, page*number);
   }
   $scope.pageChange = function(){
     $scope.pageData = _slice($scope.rooms, $scope.currPage, 5);
-    $scope.pageData = _slice($scope.rooms,$scope.curPage, 5); 
   }
   $scope.$parent.$parent.currentPage = 2;
 	apiServ.post(
 		'/eqp/api/room/all',
         {}
 	).then(
-		function (data){
-			$scope.f = data;
-			$scope._active = function (x){
-        $scope.custom = $scope.f[x]
-        console.log($scope.custom)
+     function (data){
+        $scope.rooms = data;
+        $scope.pageData = _slice($scope.rooms, 1, 5);
         apiServ.post(
-          '/eqp/api/room/all',
-          {
-            'building_id': $scope.custom.id
-          }
+          '/eqp/api/building/all',
+          {}
         ).then(
-         function (data){
-           $scope.rooms = data;
-            apiServ.post(
-                '/eqp/api/building/all',
-                {}
-              ).then(
-                function (data){            
-                  $scope.buildings = data
-                  $scope.pageData = _slice($scope.rooms,1,5)
-                  _.each($scope.rooms,function(room){
-                    var building = _.find($scope.buildings,function(building){
-                      return room.building_id === building.id;
-                    })
-                    building = building || {};
-                    room.building = building;
-                  })
-                },
-                function (err){
-                  console.log(err)
-                }
-              )
+           function (data){ 
+              $scope.buildings = data
+              $scope.pageData = _slice($scope.rooms,1,5)
+              _.each($scope.rooms,function(room){
+                var building = _.find($scope.buildings,function(building){
+                  return room.building_id === building.id;
+                })
+                building = building || {};
+                room.building = building;
+              })
+           },
+           function (err){
+            console.log(err)
           }
-		    }
-	     )
-    }
+        )
+     }
   )
 
     $scope._active = function (x) {       
@@ -62,8 +48,6 @@ angular.module('asset').controller("assetRooms",function($scope,$http,environmen
       apiServ.post(
         '/eqp/api/room/new',
         {
-        'building_id':$scope.custom.building_id,
-		    'room_code':$scope.custom.code
           	'building_id':$scope.aprent.id,
 		        'room_code':$scope.custom.code
         }
